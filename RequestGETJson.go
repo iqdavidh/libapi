5b2c2d84c98af25829d3562c12973574286d507f
iqdavidh <iq.davidh@gmail.com>
2020-05-20 05:57:23 -0500

    avance en como probar request

--- RequestGETJson.go
+++ RequestGETJson.go
@@ -1,22 +1,32 @@
 package libapi
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"net/http"
 	"strconv"
 )
 
 type DicJson map[string]interface{}
 
+type DicResp struct {
+	success bool
+	msg     string
+	data    map[string]interface{}
+}
+
 /* ************************************************************************************* */
 /* ************************************************************************************* */
-//Enviar respuesta 200 con cuento success,msg y data con el dicJson que se proporcione
+//Enviar respuesta 200 con cuento success,msg y data con el  map[string]interface{} que se proporcione
 func RespuestaSuccess(ctx *gin.Context, dic DicJson) {
-	respuesta := DicJson{
-		"success": true,
-		"msg":     "",
-		"data":    dic,
+	respuesta := DicResp{
+		true,
+		"",
+		dic,
 	}
 
 	ctx.JSON(http.StatusOK, respuesta)
@@ -24,9 +34,10 @@ func RespuestaSuccess(ctx *gin.Context, dic DicJson) {
 
 //Enviar respuesta 200 con cuento success=FALSE,msg
 func RespuestaError(ctx *gin.Context, msg string) {
-	respuesta := DicJson{
-		"success": false,
-		"msg":     msg,
+	respuesta := DicResp{
+		false,
+		msg,
+		nil,
 	}
 
 	ctx.JSON(http.StatusOK, respuesta)
@@ -34,7 +45,6 @@ func RespuestaError(ctx *gin.Context, msg string) {
 
 /* ************************************************************************************* */
 /* ************************************************************************************* */
-
 
 //Obtener un query parame integer desde ejemplo de ?param1=valorInteger
 func GetIntFromQP(ctx *gin.Context, paramName string, valorDefault uint32, isOpcional bool) (uint32, error) {
@@ -121,4 +131,47 @@ func GetIsAllCamposRequeridos(dicCampos DicJson, listaCamposReq []string) (bool,
 	}
 	return true, nil
 
+}
+
+/* *********************************************************************************************** */
+/* *********************************************************************************************** */
+/* *********************************************************************************************** */
+
+func RequestGETJson(url string, headers map[string]string) (DicJson, error) {
+
+	res, err := http.Get(url)
+	if err != nil {
+		return DicJson{}, err
+	}
+	defer res.Body.Close()
+
+	jsonBody, err2 := ioutil.ReadAll(res.Body)
+	if err2 != nil {
+		return DicJson{}, err2
+	}
+
+	//convertir body en JSON
+	dic := DicJson{}
+	errJson := json.Unmarshal(jsonBody, &dic)
+
+	if errJson != nil {
+		return DicJson{}, errJson
+	}
+
+	return dic, nil
+}
+
+func DecodeBodyResponse(Body *bytes.Buffer) (DicResp, error) {
+	body, _ := ioutil.ReadAll(Body)
+
+	if !json.Valid(body) {
+		return DicResp{}, errors.New("json inválido \n" + fmt.Sprint(body))
+	}
+	var respuesta DicResp
+	erorrJson := json.Unmarshal(body, &respuesta)
+	if erorrJson != nil {
+		return DicResp{}, errors.New("json inválido \n" + fmt.Sprint(body))
+	}
+
+	return respuesta, nil
 }
